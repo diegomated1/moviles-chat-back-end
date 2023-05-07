bfe74649c507347525aa33b679d33d7b2656e04c
diegomated1 <diegocardwr@gmail.com>
2023-05-07 16:44:09 -0500

    update insert changed all parameters for one

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -10,11 +10,11 @@ export default class MessageModel{
         this.columns = ["email_messages_addresse", "date", "email_messages_sender", "message", "title", "tokens"];
     }
 
-    get_by_addressee(email_messages_addresse:string):Promise<IMessage[]>{
+    get_by_addressee(sender:string, email_messages_addresse:string):Promise<IMessage[]>{
         return new Promise(async(res, rej)=>{
             try{
-                const query = `SELECT * FROM messages_by_addressee WHERE email_messages_addresse = ?`;
-                const query_result = await this.db.client.execute(query, [email_messages_addresse], {prepare: true});
+                const query = `SELECT * FROM messages_by_addressee WHERE email_messages_addresse = ? AND email_messages_sender = ?`;
+                const query_result = await this.db.client.execute(query, [email_messages_addresse, sender], {prepare: true});
                 const _messages = query_result.rows;
                 res(_messages.map(_message=>({
                     email_user_addresse: _message.email_messages_addresse,
@@ -30,13 +30,12 @@ export default class MessageModel{
         })
     }
 
-    insert(
-        email_messages_addresse: string, date: Date, email_messages_sender: string,
-        message: string, title: string, tokens: string[]
-    ):Promise<boolean>{
+    insert(message:IMessage):Promise<boolean>{
         return new Promise(async(res, rej)=>{
             try{
-                const params = [email_messages_addresse, date, email_messages_sender, message, title, tokens];
+                const params = [
+                    message.email_user_addresse, message.date, message.email_user_sender, message.message, message.title, message.tokens
+                ];
                 const query = `INSERT INTO messages_by_addressee (${this.columns.join(', ')}) VALUES (?, ?, ?, ?, ?, ?)`;
                 await this.db.client.execute(query, params, {prepare: true});
                 res(true);
