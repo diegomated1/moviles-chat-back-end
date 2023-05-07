42314cfaa64eb47033c20ddb3b2154082c2bb71c
diegomated1 <diegocardwr@gmail.com>
2023-05-07 17:05:39 -0500

    app

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import Server from "./server";
+import ChatRouter from "./router/chat.router";
+import UserController from "./controllers/user.controller";
+import MessageController from "./controllers/message.controller";
+import UserModel from "./models/user.model";
+import MessageModel from "./models/message.model";
+import ChatDatabase from "./database/chat.database";
+
+const database = new ChatDatabase();
+const userModel = new UserModel(database);
+const messageModel = new MessageModel(database);
+const userController = new UserController(userModel);
+const messageController = new MessageController(messageModel);
+const chatRouter = new ChatRouter(userController, messageController);
+
+const server = new Server(chatRouter);
+
