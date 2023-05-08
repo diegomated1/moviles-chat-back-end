d36cdf5ed57945e5c7f6dc33279aeb23f5f25d40
diegomated1 <diegocardwr@gmail.com>
2023-05-08 12:36:56 -0500

    using new message attribute

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -7,18 +7,19 @@ export default class MessageModel{
     columns: string[]
 
     constructor(private readonly db:ChatDatabase){
-        this.columns = ["email_messages_addresse", "date", "email_messages_sender", "message", "title", "tokens"];
+        this.columns = ["id_channel", "email_user_addresse", "date", "email_user_sender", "message", "title", "tokens"];
     }
 
-    get_by_addressee(sender:string, email_messages_addresse:string):Promise<IMessage[]>{
+    get_by_addressee(sender:string, email_user_addresse:string):Promise<Omit<IMessage, 'id_channel'>[]>{
         return new Promise(async(res, rej)=>{
             try{
-                const query = `SELECT * FROM messages_by_addressee WHERE email_messages_addresse = ? AND email_messages_sender = ?`;
-                const query_result = await this.db.client.execute(query, [email_messages_addresse, sender], {prepare: true});
+                const id_channel = [sender, email_user_addresse].sort().join("");
+                const query = `SELECT * FROM messages_by_channel WHERE id_channel = ?`;
+                const query_result = await this.db.client.execute(query, [id_channel], {prepare: true});
                 const _messages = query_result.rows;
                 res(_messages.map(_message=>({
-                    email_user_addresse: _message.email_messages_addresse,
-                    email_user_sender: _message.email.email_messages_sender,
+                    email_user_addresse: _message.email_user_addresse,
+                    email_user_sender: _message.email_user_sender,
                     title: _message.title,
                     message: _message.message,
                     date: _message.date,
@@ -30,13 +31,14 @@ export default class MessageModel{
         })
     }
 
-    insert(message:IMessage):Promise<boolean>{
+    insert(message:Omit<IMessage, 'id_channel'>):Promise<boolean>{
         return new Promise(async(res, rej)=>{
             try{
+                const id_channel = [message.email_user_sender, message.email_user_addresse].sort().join("");
                 const params = [
-                    message.email_user_addresse, message.date, message.email_user_sender, message.message, message.title, message.tokens
+                    id_channel, message.email_user_addresse, message.date, message.email_user_sender, message.message, message.title, message.tokens
                 ];
-                const query = `INSERT INTO messages_by_addressee (${this.columns.join(', ')}) VALUES (?, ?, ?, ?, ?, ?)`;
+                const query = `INSERT INTO messages_by_channel (${this.columns.join(', ')}) VALUES (?, ?, ?, ?, ?, ?, ?)`;
                 await this.db.client.execute(query, params, {prepare: true});
                 res(true);
             }catch(error){
